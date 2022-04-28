3c9d1a591a6e22dd5d2cc7eac45d47e7a7d2d7e2
fito <chanerec@gmail.com>
2022-04-28 02:01:53 -0500

    Refactorizando la generación del checksum

--- src/CheckSum.php
+++ src/CheckSum.php
@@ -6,25 +6,26 @@ namespace PhpCfdi\Rfc;
 
 final class CheckSum
 {
-    private const DICTIONARY = [0 => 0, 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14, 'F' => 15, 'G' => 16, 'H' => 17, 'I' => 18, 'J' => 19, 'K' => 20, 'L' => 21, 'M' => 22, 'N' => 23, '&' => 24, 'O' => 25, 'P' => 26, 'Q' => 27, 'R' => 28, 'S' => 29, 'T' => 30, 'U' => 31, 'V' => 32, 'W' => 33, 'X' => 34, 'Y' => 35, 'Z' => 36, ' ' => 37, '#' => 38];
+    private const DICTIONARY = '0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ #';
 
     public function calculate(string $rfc): string
     {
         // 'Ñ' translated to '#' because 'Ñ' is multibyte 0xC3 0xB1
-        $chars = str_split(str_replace('Ñ', '#', $rfc), 1);
-        array_pop($chars); // remove predefined checksum
+        $chars = str_split(str_replace('Ñ', '#', $rfc));
         $length = count($chars);
-        $sum = (11 === $length) ? 481 : 0; // 481 para morales, 0 para físicas
-        $j = $length + 1;
+        array_pop($chars); // remove predefined checksum
+        $sum = (12 === $length) ? 481 : 0; // 481 para morales, 0 para físicas
         foreach ($chars as $i => $char) {
-            $sum += self::DICTIONARY[$char] * ($j - $i);
+            $posChar = strpos(self::DICTIONARY, $char);
+            $factor = $length - $i;
+            $sum += $posChar * $factor;
         }
-        $digit = strval(11 - $sum % 11);
-        if ('11' === $digit) {
-            $digit = '0';
-        } elseif ('10' === $digit) {
-            $digit = 'A';
+        $mod11 = $sum % 11;
+        if ($mod11 === 0) {
+          return '0';
+        } else if($mod11 === 1) {
+          return 'A';
         }
-        return $digit;
+        return strval(11 - $mod11);
     }
 }
